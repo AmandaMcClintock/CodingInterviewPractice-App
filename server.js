8736edd6fcb804ed00e793028400dcb38cc0da6d
Amanda McClintock <amanda.d.mcclintock@gmail.com>
2017-11-18 21:22:54 -0500

    Chapter 5

---
+++ server.js
@@ -0,0 +1,37 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+
+var app = express()
+app.use(bodyParser.json())
+
+app.get('/api/posts', function(req, res) {
+    res.json([
+        {
+            username :  'Amanda',
+            body : 'I\'m tryin\''
+        }
+    ])
+})
+
+app.listen(3000, function() {
+        console.log('Server listening on', 3000)
+})
+
+//Using Mongoose models with the post endpoint
+var Post = require('./models/post')
+app.post('/api/posts', function(req, res, next) {
+    var post = new Post({
+        username: req.body.username,
+        body: req.body.body
+    })
+
+    post.save(function(err, post) {
+        if(err) { return next(err) }
+        res.json(201, post)
+    })
+})
+
+/*
+ * To test use curl -v -H "Content-Type: application/json" -XPOST --data
+ * Alternatively, test in brower with http://localhost:3000/api/posts
+ */
