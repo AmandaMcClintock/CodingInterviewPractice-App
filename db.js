a67e4c4f61b66c93b52a4bc48ea7bb4667c1cd9d
Amanda McClintock <amcclintock@mac-amcclintock1.cb.careerbuilder.com>
2017-11-20 12:51:31 -0500

    Chapter 5

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/social', function() {
+mongoose.connect('mongodb://localhost/SocialNetwork', function() {
     console.log('mongodb connected')
 })
 
