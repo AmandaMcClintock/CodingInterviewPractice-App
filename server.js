4b5e11a67fe49d8da8ae1340b1374fb0a216be6d
Amanda McClintock <amcclintock@mac-amcclintock1.cb.careerbuilder.com>
2017-11-21 09:51:23 -0500

    api works

--- server.js
+++ server.js
@@ -2,27 +2,41 @@ var express     = require('express')
 var bodyParser  = require('body-parser')
 var Post        = require('./models/post')
 
-var app         = express()
-
+var app = express()
 app.use(bodyParser.json())
+
+/*
 app.get('/api/posts', function(req, res, next) {
     Post.find(function(err, posts) {
         if(err) { return next(err) }
         res.json(posts)
     })
 })
+*/
+app.get('/api/posts', function(req, res) {
+    res.json([
+        {
+            username: 'Amanda McCliintock',
+            body: 'Anything really. Just work.'
+        }
+    ])
+})
 
 //Using Mongoose models with the post endpoint
 app.post('/api/posts', function(req, res, next) {
-    var post = new Post({
-        username: req.body.username,
-        body: req.body.body
-    })
-    post.save(function(err, post) {
-        if(err) { return next(err) }
-        res.send(201).json(post)
-    })
+    console.log('post received')
+    console.log(req.body.username)
+    console.log(req.body.body)
+    // var post = new Post({
+    //     username: req.body.username,
+    //     body: req.body.body
+    // })
+    // post.save(function(err, post) {
+    //     if(err) { return next(err) }
+         res.send(201)//.json(post)
+    // })
 })
+
 app.listen(3000, function() {
     console.log('Server listening on', 3000)
 })
