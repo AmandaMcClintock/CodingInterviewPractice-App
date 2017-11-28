43d6c9af3f6d30fa2ba027ea1eb927c2ab7006b4
Amanda McClintock <amcclintock@mac-amcclintock1.cb.careerbuilder.com>
2017-11-28 14:52:00 -0500

    Chapter 6: Gulp Dev Tasks

--- gulp/server.js
+++ gulp/server.js
@@ -1,6 +1,15 @@
 var gulp    = require('gulp')
 var nodemon = require('gulp-nodemon')
 
+//restarts server on gulp dev:server
+gulp.take('dev:server', function() {
+  nodemon({
+    script: 'server.js',
+    ext: 'js'
+  })
+})
+
+//ignores non node files
 gulp.task('dev:server', function () {
   nodemon({
     script: 'server.js',
@@ -8,3 +17,5 @@ gulp.task('dev:server', function () {
     ignore: ['ng*', 'gulp*', 'assets*']
   })
 })
+
+gulp.task('dev', ['watch:css', 'watch:js', 'dev:server'])
