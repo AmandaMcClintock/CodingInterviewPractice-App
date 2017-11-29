4f21d210d5479e01ff65179b782fb8ca931d5fea
Amanda McClintock <amanda.d.mcclintock@gmail.com>
2017-11-28 20:58:45 -0500

    Chapter-6: Removed redundant code

--- gulp/server.js
+++ gulp/server.js
@@ -1,14 +1,6 @@
 var gulp    = require('gulp')
 var nodemon = require('gulp-nodemon')
 
-//restarts server on gulp dev:server
-gulp.task('dev:server', function() {
-  nodemon({
-    script: 'server.js',
-    ext: 'js'
-  })
-})
-
 //ignores non node files
 gulp.task('dev:server', function () {
   nodemon({
