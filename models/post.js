b6978761a1fdf3f9f83b46d3ccd4b2b520a6f9a1
Amanda McClintock <amcclintock@mac-amcclintock1.cb.careerbuilder.com>
2017-11-29 11:55:04 -0500

    Chapter 5: Correctedvariable and fixed formatting

--- models/post.js
+++ models/post.js
@@ -1,7 +1,7 @@
 var db = require('../db')
 var Post = db.model('Post', {
-    username: { type: String, require: true },
+    username: { type: String, required: true },
     body:     { type: String, required: true },
-    date:     { type: Date, required: true, default: Date.now }
+    date:     { type: Date,   required: true, default: Date.now }
 })
 module.exports = Post
